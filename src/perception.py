d87ced8301fae5a5a51a8601a71f82773839c8ed
ashkank <ashkank@umich.edu>
2023-05-01 10:36:27 -0400

    filtering out unimportant fields

--- src/perception.py
+++ src/perception.py
@@ -32,46 +32,100 @@ def groupByWorkerId(data):
         grouped[row['WorkerId']].append(row)
     return grouped
 
+# Input: a dict of HITS mapped to their corresponding WorkerIds
+# Output: a new dict of WorkerIds mapped to their corresponding HITS, while only preserving the columns that are most important
+# List of columns that are important:
+# Answer.10mo2a
+# Answer.10mo2b
+# Answer.10mo2ab
+# Answer.10mo2c
+# Answer.2imply.yes
+# Answer.mo2a
+# Answer.mo2b
+# Answer.mo2c
+# Answer.3imply.yes
+# Answer.3mo2a
+# Answer.3mo2b
+# Answer.3mo2c
+# Answer.4imply.yes
+# Answer.4mo2a
+# Answer.4mo2b
+# Answer.4mo2c
+# Answer.5imply.yes
+# Answer.5mo2a
+# Answer.5mo2b
+# Answer.5mo2c
+# Answer.6imply.yes
+# Answer.6mo2a
+# Answer.6mo2b
+# Answer.6mo2c
+# Answer.7imply.yes
+# Answer.7mo2a
+# Answer.7mo2b
+# Answer.7mo2c
+# Answer.8imply.yes
+# Answer.8mo2a
+# Answer.8mo2b
+# Answer.8mo2c
+# Answer.likert.agree
+# Answer.likert.disagree
+# Answer.likert.neutral
+# Answer.likert.strong_agree
+# Answer.likert.strong_disagree
+# Answer.real.mis
+# Answer.real.realnews
+def filterColumns(groupedMTurkData):
+    filtered = {}
+    for workerId in groupedMTurkData:
+        filtered[workerId] = []
+        for row in groupedMTurkData[workerId]:
+            filtered[workerId].append({
+                'Answer.10mo2a': row['Answer.10mo2a'],
+                'Answer.10mo2b': row['Answer.10mo2b'],
+                'Answer.10mo2ab': row['Answer.10mo2ab'],
+                'Answer.10mo2c': row['Answer.10mo2c'],
+                'Answer.2imply.yes': row['Answer.2imply.yes'],
+                'Answer.mo2a': row['Answer.mo2a'],
+                'Answer.mo2b': row['Answer.mo2b'],
+                'Answer.mo2c': row['Answer.mo2c'],
+                'Answer.3imply.yes': row['Answer.3imply.yes'],
+                'Answer.3mo2a': row['Answer.3mo2a'],
+                'Answer.3mo2b': row['Answer.3mo2b'],
+                'Answer.3mo2c': row['Answer.3mo2c'],
+                'Answer.4imply.yes': row['Answer.4imply.yes'],
+                'Answer.4mo2a': row['Answer.4mo2a'],
+                'Answer.4mo2b': row['Answer.4mo2b'],
+                'Answer.4mo2c': row['Answer.4mo2c'],
+                'Answer.5imply.yes': row['Answer.5imply.yes'],
+                'Answer.5mo2a': row['Answer.5mo2a'],
+                'Answer.5mo2b': row['Answer.5mo2b'],
+                'Answer.5mo2c': row['Answer.5mo2c'],
+                'Answer.6imply.yes': row['Answer.6imply.yes'],
+                'Answer.6mo2a': row['Answer.6mo2a'],
+                'Answer.6mo2b': row['Answer.6mo2b'],
+                'Answer.6mo2c': row['Answer.6mo2c'],
+                'Answer.7imply.yes': row['Answer.7imply.yes'],
+                'Answer.7mo2a': row['Answer.7mo2a'],
+                'Answer.7mo2b': row['Answer.7mo2b'],
+                'Answer.7mo2c': row['Answer.7mo2c'],
+                'Answer.8imply.yes': row['Answer.8imply.yes'],
+                'Answer.8mo2a': row['Answer.8mo2a'],
+                'Answer.8mo2b': row['Answer.8mo2b'],
+                'Answer.8mo2c': row['Answer.8mo2c'],
+                'Answer.likert.agree': row['Answer.likert.agree'],
+                'Answer.likert.disagree': row['Answer.likert.disagree'],
+                'Answer.likert.neutral': row['Answer.likert.neutral'],
+                'Answer.likert.strong_agree': row['Answer.likert.strong_agree'],
+                'Answer.likert.strong_disagree': row['Answer.likert.strong_disagree'],
+                'Answer.real.mis': row['Answer.real.mis'],
+                'Answer.real.realnews': row['Answer.real.realnews']
+            })
+    return filtered
+
 
 if __name__ == '__main__':
-    # Read in the data
-    # data = readTSV('../data/mrf_v1/train_annot_all.tsv')
-
-    # Print out the data
-    # print(data[:5])
     mTurkData = readCSVFiles('../data/mturk_data/')
     print("Number of HITs completed: ", len(mTurkData))
     groupedMTurkData = groupByWorkerId(mTurkData)
-
-
-
-    # select a random sample of 50 keys from groupedMTurkData, and print the length of the value list for each key
-    # print("Random sample of 50 workers:")
-    # for key in random.sample(list(groupedMTurkData.keys()), 50):
-    #     example = random.sample(groupedMTurkData[key], 1)[0]
-    #     # print(example['Input.sentence'], len(groupedMTurkData[key]))
-    #     print(example['Input.sentence'])
-    #
-    # print("---------------")
-
-    # sort the keys by the length of the value list, and print the first 50 keys descending
-    # print("Top 50 workers by number of HITs completed:")
-    # for key in sorted(groupedMTurkData, key=lambda k: len(groupedMTurkData[k]), reverse=True)[:50]:
-    #     print(len(groupedMTurkData[key]))
-
-    # print all rows in mTurkData where the value for 'Input.reason' is longer than 30 characters and 'LifetimeApprovalRate' is over 90%
-    # all_column_names = list(set([key for row in mTurkData for key in row.keys()]))
-    # for idx, row in enumerate(mTurkData):
-    #     for column in all_column_names:
-    #         if column not in row:
-    #             mTurkData[idx][column] = ''
-
-    # print(len([row for row in mTurkData if len(row['Input.reason']) > 20]))
-    # print(len(set([row['Input.reason'].lower() for row in mTurkData if len(row['Input.reason']) > 20])))
-    # for row in mTurkData:
-    #     if len(row['Input.reason']) > 10 and len(row['LifetimeApprovalRate']) > 0 and float(row['LifetimeApprovalRate'].split('%')[0]) >= 90:
-    #         print('WorkerId: {}'.format(row['WorkerId']))
-    #         print('sentence: {}'.format(row['Input.sentence']))
-    #         print('reason: {}'.format(row['Input.reason']))
-    #         print('-----------------')
-    # print(sorted(all_column_names))
+    refinedGroupedMTurkData = filterColumns(groupedMTurkData)
+    print("A random sample of 10 HITS completed by a random worker: ", random.sample(refinedGroupedMTurkData[random.choice(list(refinedGroupedMTurkData.keys()))], 10))
