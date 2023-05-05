99d7912d6b276c075c192c4bf8c299a23eb8a98a
ashkan <ashkank@umich.edu>
2023-05-05 10:50:06 -0400

    WIP: implementing data filtering and cleaning

--- src/humanWorker.py
+++ src/humanWorker.py
@@ -1,8 +1,15 @@
 from .worker import Worker
 class HumanWorker(Worker):
 
-        def __init__(self, id, demographics, mediaConsumptionRegime):
+        def __init__(self, id, ageBracket, education, ethnicity, gender, mediaConsumptionRegime):
             super().__init__(id)
-            self.demographics = demographics
+            self.ageBracket = ageBracket
+            self.education = education
             self.mediaConsumptionRegime = mediaConsumptionRegime
+            self.ethnicity = ethnicity
+            self.gender = gender
+            self.annotatedFrames = []
 
+
+        def addFrame(self, frame):
+            self.annotatedFrames.append(frame)
--- src/perception.py
+++ src/perception.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     print("Number of HITs completed: ", len(mTurkData))
     groupedMTurkData = mrfdu.groupByWorkerId(mTurkData)
     refinedGroupedMTurkData = mrfdu.filterColumns(groupedMTurkData)
-    print("A random sample of 10 HITS completed by a random worker: ", random.sample(refinedGroupedMTurkData[random.choice(list(refinedGroupedMTurkData.keys()))], 10))
+    print(random.sample(refinedGroupedMTurkData[random.choice(list(refinedGroupedMTurkData.keys()))], 1))
 
