f7dcdc42f25d05298e9dd07ab2af12d30b94a6ec
ashkan <ashkank@umich.edu>
2023-05-09 09:14:37 -0400

    MRF data cleaning tested and done

--- src/humanWorker.py
+++ src/humanWorker.py
@@ -6,7 +6,17 @@ class HumanWorker(Worker):
 
         def __init__(self, id, demographics, mediaConsumptionRegimen, approvalRating):
             super().__init__(id)
-            self.demographics = demographics
+            # self.demographics = demographics
+
+            self.age = HumanWorker.removeDefaultValues(demographics['age'], '0')
+            self.age = self.age[0] if len(self.age) else None
+
+            self.gender = HumanWorker.removeDefaultValues(demographics['gender'], '0') if len(demographics['gender']) > 1 else demographics['gender'][0] if len(demographics['gender'][0]) else None
+
+            self.education = HumanWorker.removeDefaultValues(demographics['education'], '0') if len(demographics['education']) > 1 else demographics['education'][0] if len(demographics['education'][0]) else None
+
+            self.race = demographics['race']
+
             self.mediaConsumptionRegimen = mediaConsumptionRegimen
             self.approvalRating = approvalRating
             self.annotatedFrames = []
@@ -22,3 +32,8 @@ class HumanWorker(Worker):
         @staticmethod
         def _decode(humanWorkerDict):
             return namedtuple('X', humanWorkerDict.keys())(*humanWorkerDict.values())
+
+
+        @staticmethod
+        def removeDefaultValues(attribute, defaultValue):
+            return [x for x in attribute if x != defaultValue]
--- src/perception.py
+++ src/perception.py
@@ -6,3 +6,12 @@ from utils import saveObjectsToJsonFile, loadObjectsFromJsonFile
 if __name__ == '__main__':
     # workers = mrfdu.loadAndCleanMRFDataset('../data/mturk_data/', '../data/mrf_turk_processed.json')
     workers = loadObjectsFromJsonFile('../data/mrf_turk_processed.json')
+    workers = sorted(workers, key=lambda x: len(x['annotatedFrames']), reverse=True)
+
+    workersWDemographics = 0
+    for worker in workers:
+        print(
+            f'ID: {worker["id"]}, Age: {worker["age"]}, Education: {worker["education"]}, Gender: {worker["gender"]}, '
+            f'Race: {worker["race"]}, #Annotations: {len(worker["annotatedFrames"])}'
+        )
+        print('------------------')
