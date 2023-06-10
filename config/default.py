fa6d40d3cb3d7ed3fa478ce4c73a676b942e862a
ashkan <ashkank@umich.edu>
2023-06-10 12:50:10 -0400

    WIP: setting up a training script, changing config files so they support training scripts

--- config/default.py
+++ config/default.py
@@ -1,7 +1,38 @@
-BASE_MODEL_NAME = "google/flan-t5-small" # better to load model locally
-FROZEN_LAYER_DEPTH_THRESHOLD = 70000000 # todo this is model dependent, fix it before training
-BATCH_SIZE = 8
-MAX_INPUT_LENGTH = 512
-MAX_OUTPUT_LENGTH = 128
-DATASET_PATH = '../data/trajectories/1_initial/'
-MODEL_PATH = '../models/1_initial/'
+from types import SimpleNamespace
+
+
+default = SimpleNamespace(
+BASE_MODEL_NAME = "google/flan-t5-small",
+BATCH_SIZE = 8,
+MAX_INPUT_LENGTH = 512,
+MAX_OUTPUT_LENGTH = 512,
+DATASET_PATH = '../data/trajectories/1_initial/',
+MODEL_PATH = '../models/1_initial/',
+)
+
+bigDataSmallModel = SimpleNamespace(
+BASE_MODEL_NAME = "google/flan-t5-small",
+BATCH_SIZE = 8,
+MAX_INPUT_LENGTH = 512,
+MAX_OUTPUT_LENGTH = 512,
+DATASET_PATH = '../data/trajectories/big/',
+MODEL_PATH = '../models/bigDataSmallModel/',
+)
+
+bigDataMediumModel = SimpleNamespace(
+BASE_MODEL_NAME = "google/flan-t5-base",
+BATCH_SIZE = 8,
+MAX_INPUT_LENGTH = 512,
+MAX_OUTPUT_LENGTH = 512,
+DATASET_PATH = '../data/trajectories/big/',
+MODEL_PATH = '../models/bigDataMediumModel/',
+)
+
+bigDataBigModel = SimpleNamespace(
+BASE_MODEL_NAME = "google/flan-t5-large",
+BATCH_SIZE = 8,
+MAX_INPUT_LENGTH = 512,
+MAX_OUTPUT_LENGTH = 512,
+DATASET_PATH = '../data/trajectories/big/',
+MODEL_PATH = '../models/bigDataBigModel/',
+)
---
+++ src/training_script.sh
@@ -0,0 +1,4 @@
+python train_perception_modeling.py default &&
+python train_perception_modeling.py bigDataSmallModel &&
+python train_perception_modeling.py bigDataMediumModel &&
+python train_perception_modeling.py bigDataBigModel
