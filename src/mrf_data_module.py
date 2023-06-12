aaf95fa9f463644a9b0a743134e79717a0411a14
ashkank <ashkank@umich.edu>
2023-06-12 13:41:23 -0400

    doing another run of training, after server power issue

--- src/mrf_data_module.py
+++ src/mrf_data_module.py
@@ -73,4 +73,4 @@ class MRFDataModule(pl.LightningDataModule):
     def _wrapInDatasetObj(self, fileName):
         transformedData = MRFDataset(self.datasetPath + fileName, self.config)
         # transformedData.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
-        return DataLoader(transformedData, batch_size=self.batchSize, shuffle=False, num_workers=1, pin_memory=True)
+        return DataLoader(transformedData, batch_size=self.batchSize, shuffle=False, num_workers=3, pin_memory=True)
