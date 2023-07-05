3721b151c57a8ce8d061f33f5945e61912b1a9c9
ashkan <ashkank@umich.edu>
2023-07-05 10:55:21 -0400

    training script for first batch of reliable evaluations

--- src/training_script.sh
+++ src/training_script.sh
@@ -1,8 +1,8 @@
 #python train_perception_modeling.py bigDataSmallModel gen &&
-#python evaluate_perception_modeling.py bigDataSmallModel &&
+python evaluate_perception_modeling.py bigDataSmallModel &&
 #python train_perception_modeling.py bigDataSmallModelLabelsOnly gen &&
-#python evaluate_perception_modeling.py bigDataSmallModelLabelsOnly
+python evaluate_perception_modeling.py bigDataSmallModelLabelsOnly &&
 #python train_perception_modeling.py bigDataMediumModelLabelsOnly load &&
 #python evaluate_perception_modeling.py bigDataMediumModelLabelsOnly
-python train_perception_modeling.py bigDataLongMediumModel load &&
+#python train_perception_modeling.py bigDataLongMediumModel load &&
 python evaluate_perception_modeling.py bigDataLongMediumModel
