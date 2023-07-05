dee2f1eb2f5fe97eb902a683836f45806222b315
ashkan <ashkank@umich.edu>
2023-07-05 11:05:19 -0400

    updating training_script.sh

--- src/training_script.sh
+++ src/training_script.sh
@@ -1,8 +1,8 @@
-#python train_perception_modeling.py bigDataSmallModel gen &&
-python evaluate_perception_modeling.py bigDataSmallModel &&
-#python train_perception_modeling.py bigDataSmallModelLabelsOnly gen &&
-python evaluate_perception_modeling.py bigDataSmallModelLabelsOnly &&
+CUDA_VISIBLE_DEVICES=2,3 python train_perception_modeling.py bigDataSmallModel load &&
+CUDA_VISIBLE_DEVICES=3 python evaluate_perception_modeling.py bigDataSmallModel &&
+CUDA_VISIBLE_DEVICES=2,3 python train_perception_modeling.py bigDataSmallModelLabelsOnly load &&
+CUDA_VISIBLE_DEVICES=3 python evaluate_perception_modeling.py bigDataSmallModelLabelsOnly &&
 #python train_perception_modeling.py bigDataMediumModelLabelsOnly load &&
 #python evaluate_perception_modeling.py bigDataMediumModelLabelsOnly
 #python train_perception_modeling.py bigDataLongMediumModel load &&
-python evaluate_perception_modeling.py bigDataLongMediumModel
+CUDA_VISIBLE_DEVICES=3 python evaluate_perception_modeling.py bigDataLongMediumModel
