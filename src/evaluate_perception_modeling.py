24dc9b527b6efcd4552f1de4a587e062cc01c207
ashkan <ashkank@umich.edu>
2023-07-10 11:15:47 -0400

    refactoring eval code

--- src/evaluate_perception_modeling.py
+++ src/evaluate_perception_modeling.py
@@ -8,6 +8,20 @@ import sys
 import trainingConfigs
 
 
+def evaluate(trainingConfig):
+    trainingConfig.BATCH_SIZE *= 8
+    # trainingConfig.MODEL_PATH = '/local2/ashkank/perception/trainedModels/bigDataLongMediumModel/'
+    # trainingConfig.DATASET_PATH = '/local2/ashkank/perception/data/trajectories/big/'
+    mrf = MRFDataModule(trainingConfig)
+    model = MisinfoPerceptionT5(trainingConfig, len(mrf.test_dataloader()) // trainingConfig.BATCH_SIZE,
+                                loadLocally=True, localModelPath=trainingConfig.MODEL_PATH + 'trained_model/')
+    model.testSetJson = mrf.test_dataloader().dataset.jsonData
+    trainer = Trainer(accelerator='cuda', devices='auto')
+    # trainer = Trainer(accelerator='cuda', devices=[3])
+    trainer.test(model, datamodule=mrf)
+    yPred, yTrue = model.reduceTestResults()
+    print(accuracy_score(yTrue, yPred), f1_score(yTrue, yPred, pos_label=0), f1_score(yTrue, yPred, pos_label=1))
+
 
 if __name__ == '__main__':
 
@@ -22,18 +36,4 @@ if __name__ == '__main__':
     if trainingConfig is None:
         raise Exception("Invalid config name: " + configName)
 
-    trainingConfig.BATCH_SIZE *= 8
-    # trainingConfig.MODEL_PATH = '/local2/ashkank/perception/trainedModels/bigDataLongMediumModel/'
-    # trainingConfig.DATASET_PATH = '/local2/ashkank/perception/data/trajectories/big/'
-
-    mrf = MRFDataModule(trainingConfig)
-    model = MisinfoPerceptionT5(trainingConfig, len(mrf.test_dataloader()) // trainingConfig.BATCH_SIZE, loadLocally=True, localModelPath=trainingConfig.MODEL_PATH + 'trained_model/')
-    model.testSetJson = mrf.test_dataloader().dataset.jsonData
-
-    trainer = Trainer(accelerator='cuda', devices='auto')
-    # trainer = Trainer(accelerator='cuda', devices=[3])
-    trainer.test(model, datamodule=mrf)
-
-    yPred, yTrue = model.reduceTestResults()
-
-    print(accuracy_score(yTrue, yPred), f1_score(yTrue, yPred, pos_label=0), f1_score(yTrue, yPred, pos_label=1))
+    evaluate(trainingConfig)
