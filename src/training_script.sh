883c3a76d607b52bb4f8437937be3ba1b382d863
ashkank <ashkank@umich.edu>
2023-09-29 10:19:30 -0400

    rerunning 10 fold cv experiments

--- src/training_script.sh
+++ src/training_script.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1,3 python hot_start_cv_training.py HotStartCV gen &&
+CUDA_VISIBLE_DEVICES=1,3 python hot_start_cv_training.py HotStartCV load &&
 CUDA_VISIBLE_DEVICES=3 python hot_start_cv_evaluation.py HotStartCV
