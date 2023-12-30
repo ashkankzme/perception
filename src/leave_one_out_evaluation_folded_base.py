3bf8ff0041bde78e974146c9d92d28b5d4037b84
ashkank <ashkank@umich.edu>
2023-12-30 10:29:50 -0500

    extra evaluation for thesis rewrites: doing a folded evaluation of the base model in the zero shot experiments

---
+++ src/leave_one_out_evaluation_folded_base.py
@@ -0,0 +1,32 @@
+from evaluate_perception_modeling import evaluate, parseEvalConfig
+from mrf_data_module import MRFDataModule
+from utils import loadObjectsFromJsonFile, saveToJsonFile
+import sys
+
+if __name__ == '__main__':
+    evalConfig = parseEvalConfig(sys.argv)
+    evalConfig.BATCH_SIZE *= 8
+
+    print("Loading worker ids...")
+    workerIdsWDemographics = loadObjectsFromJsonFile('../data/worker_ids_with_demographics.json')
+    results = []
+    # evalConfig.MODEL_PATH = '/local2/ashkank/perception/trainedModels/leaveOneOut'
+    # evalConfig.DATASET_PATH = '/local2/ashkank/perception/data/trajectories/leaveoneout/'
+
+    for workerId in workerIdsWDemographics:
+        print("Evaluating the leave one out model for worker " + workerId + "...")
+        mrf = MRFDataModule(evalConfig, excludedWorkers=[wId for wId in workerIdsWDemographics if wId != workerId])
+        acc, f1_misinfo, f1_real = evaluate(evalConfig, mrf, evalConfig.MODEL_PATH + '_base/trained_model')
+        results.append({workerId: [acc, f1_misinfo, f1_real]})
+        print("Worker " + workerId + " model evaluation results: ", acc, f1_misinfo, f1_real)
+
+        print("Evaluating the leave one out model for worker " + workerId + " with masked demographics...")
+        setattr(evalConfig, "MASKED_DEMOGRAPHICS", True)
+        mrf = MRFDataModule(evalConfig, excludedWorkers=[wId for wId in workerIdsWDemographics if wId != workerId])
+        acc, f1_misinfo, f1_real = evaluate(evalConfig, mrf, evalConfig.MODEL_PATH + '_base/trained_model')
+        results[-1][workerId] += [acc, f1_misinfo, f1_real]
+        print("Worker " + workerId + " model evaluation results with masked demographics: ", acc, f1_misinfo, f1_real)
+        setattr(evalConfig, "MASKED_DEMOGRAPHICS", False)
+        print('-' * 25)
+
+    saveToJsonFile(results, evalConfig.DATASET_PATH + 'test_results_loo_folded_base.json')
